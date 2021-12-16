7700bed943ebf2cf28a276b9c8267c2750199593
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-16 16:49:57 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Requêtes_Faciles___.NET__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Entiers = new List<int> { 1, 2, 3, 4, 5, 6, 2, 8, 9, 45, 12, 3, 47 };
+
+            // Query Method 
+
+            var query = from x in Entiers where x > 5 select x;
+
+            foreach (var x in query)
+                  // if (x > 5)
+                    Console.WriteLine(x);
+        }
+    }
+}
